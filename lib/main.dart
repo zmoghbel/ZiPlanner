87719075c9191ca0f0d55ce0e682c939e821ecf6
Zahra Adelung <zahra.adelung@gmail.com>
2020-11-30 10:17:28 +0100

    initial route

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,17 @@ import 'package:ziplanner/pages/home-page.dart';
 import 'package:ziplanner/pages/todo-add-page.dart';
 
 void main() {
-  runApp(new CupertinoApp(
-    initialRoute: '/home',
-    routes: {
-      '/': (context) => Home(),
-      '/home': (context) => Home(),
-      '/todoAdd': (context) => ToDoAdd(),
-    },
-    localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-      DefaultMaterialLocalizations.delegate,
-      DefaultWidgetsLocalizations.delegate,
-    ],
-  ));
+  runApp(
+    new CupertinoApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/todoAdd': (context) => ToDoAdd(),
+      },
+      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
+    ),
+  );
 }
